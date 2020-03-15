9148c728b7a71e55328c558310bf19a18e422dbc
greg hawthorne <apple@Apples-MacBook-Air.local>
2020-03-15 12:03:35 -0700

    add twitter card

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -25,6 +25,12 @@ const SEO = ({ title, description }) => {
   <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
     <meta name="description" content={description || siteDesc} />
     <meta name="image" content={image} />
+    {/* twitter card */}
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta name='twitter: creator' content={twitteUsername}/>
+    <meta name='twitter: description' content={description}/>
+    <meta name='twitter: title' content={siteTitle}/>
+    <meta name='twitte: image' content={`${siteUrl}${image}`}/>
   </Helmet>
 )
 }
