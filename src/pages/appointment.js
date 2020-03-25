e997719ae78b06801e0554bcebefdbf204a472b6
greg hawthorne <apple@Apples-MacBook-Air.local>
2020-03-24 18:23:26 -0700

    add calendly

--- src/pages/appointment.js
+++ src/pages/appointment.js
@@ -2,19 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 import "../css/appointment.module.css"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import CalendlyEmbed from "../components/CalendlyEmbedReact"
 import SEO from "../components/SEO"
 
 const Appointment = () => (
   <Layout>
-   <section className="section">
+
     <SEO title="appointments"/>
     <h1>Lets Get Started</h1>
     <CalendlyEmbed className="calendly"account="greghawthorne" eventName="discovery-meeting" />
     <Link to="/">Go back to the homepage</Link>
 
-   </section>
   </Layout>
 )
 
